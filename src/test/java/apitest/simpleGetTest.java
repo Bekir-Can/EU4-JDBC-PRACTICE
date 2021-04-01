11059a9ee383a842b2294d2b2666454f537846e3
Bekir-Can <ruikosta03@gmail.com>
2021-04-01 21:26:43 +0300

    updated1

--- src/test/java/apitest/simpleGetTest.java
+++ src/test/java/apitest/simpleGetTest.java
@@ -6,6 +6,10 @@ import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.given;
+
+
 public class simpleGetTest {
 
     String hrUrl = "http://34.228.41.120:1000/ords/hr/regions";
@@ -45,12 +49,38 @@ public class simpleGetTest {
         //to verify content type is json
         Assert.assertEquals(response.contentType(),"application/json");
 
+    }
+    @Test
+    public void test3(){ //in one line, coverted the same above the test2 case
 
-
+        RestAssured.given().accept(ContentType.JSON)
+                            .when().get(hrUrl).then()
+                            .assertThat().statusCode(200)
+                            .and().contentType("application/json");
 
     }
 
+     /*
+        Given accept type is json
+        When user sends get request to regions/2
+        Then response status code must be 200
+        and body is json format
+        and response body contains Americas
+     */
 
+    @Test
+    public void test4(){
+        Response response = given().accept(ContentType.JSON)
+                            .when().get(hrUrl + "/2");
 
+        //verify status code
+        Assert.assertEquals(response.getStatusCode(),200);
+
+        //verify content type
+        Assert.assertEquals(response.contentType(),"application/json");
+
+        //verify body contains Americas
+        Assert.assertTrue(response.body().asString().contains("Americas"));
+    }
 
 }
