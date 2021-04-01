c871bddbd448ad7519fa9299cc19564cd4d4d3cc
Bekir-Can <ruikosta03@gmail.com>
2021-04-01 18:37:30 +0300

    updated

--- src/test/java/apitest/simpleGetTest.java
+++ src/test/java/apitest/simpleGetTest.java
@@ -1,7 +1,9 @@
 package apitest;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class simpleGetTest {
@@ -14,7 +16,41 @@ public class simpleGetTest {
 
         Response response = RestAssured.get(hrUrl);
 
+        //response.statusCode(); => to verify response code
+        System.out.println("response.statusCode() = " + response.statusCode());
+
+        //response.prettyPrint(); => to print the json body
+        System.out.println("response.prettyPrint() = " + response.prettyPrint());
+
+
     }
+    /*
+     Given accept type is json
+     When user sends get request to regions endpoint
+     Then response status code must be 200
+     and body is json format
+    */
+
+    //Now how to automate the above the sample!!! Follw the below..
+    @Test
+    public void test2(){
+
+        Response response = RestAssured.given().accept(ContentType.JSON).when().get(hrUrl);
+
+        // to verify response status code is 200
+        Assert.assertEquals(response.statusCode(),200);
+
+        System.out.println("response.contentType() = " + response.contentType());
+
+        //to verify content type is json
+        Assert.assertEquals(response.contentType(),"application/json");
+
+
+
+
+    }
+
+
 
 
 }
