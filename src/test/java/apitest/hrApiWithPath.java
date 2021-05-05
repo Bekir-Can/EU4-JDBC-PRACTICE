65db4bab46075e0849ad3839ea50d3ac8a0923c2
Bekir-Can <ruikosta03@gmail.com>
2021-05-05 15:04:20 +0300

    added hrApiWithJsonPath

--- src/test/java/apitest/hrApiWithPath.java
+++ src/test/java/apitest/hrApiWithPath.java
@@ -14,6 +14,7 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class hrApiWithPath {
+
     @BeforeClass
     public void beforeclass(){
         baseURI= ConfigurationReader.get("hr_api_url");
